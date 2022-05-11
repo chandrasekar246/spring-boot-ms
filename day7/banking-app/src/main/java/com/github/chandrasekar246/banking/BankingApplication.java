252cca90a59929827c3a120ae71961677eb6f404
Chandrasekar S <chandrasekar246@yahoo.co.in>
2022-05-11 16:06:20 +0530

    log config

--- day7/banking-app/src/main/java/com/github/chandrasekar246/banking/BankingApplication.java
+++ day7/banking-app/src/main/java/com/github/chandrasekar246/banking/BankingApplication.java
@@ -1,14 +1,21 @@
 package com.github.chandrasekar246.banking;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankingApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BankingApplication.class);
 
 	public static void main(String[] args) {
+		
+		LOGGER.info("********** Application started... **********");
+		
 		SpringApplication.run(BankingApplication.class, args);
 	}
 
