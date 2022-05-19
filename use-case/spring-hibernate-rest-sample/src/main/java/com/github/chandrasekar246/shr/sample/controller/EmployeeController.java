601b05669494c0c186e72bc14715744182e85d23
Chandrasekar S <chandrasekar246@yahoo.co.in>
2022-05-19 18:26:30 +0530

    Enhanced EmployeeController

--- use-case/spring-hibernate-rest-sample/src/main/java/com/github/chandrasekar246/shr/sample/controller/EmployeeController.java
+++ use-case/spring-hibernate-rest-sample/src/main/java/com/github/chandrasekar246/shr/sample/controller/EmployeeController.java
@@ -3,9 +3,15 @@ package com.github.chandrasekar246.shr.sample.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +25,13 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeServiceImpl employeeService;
 
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HttpStatus> addEmployee(@RequestBody Employee employee) {
+		employeeService.addEmployee(employee);
+
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Employee> listEmployees() {
 		return employeeService.listEmployees();
@@ -27,6 +40,16 @@ public class EmployeeController {
 	@GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Employee getEmployeeById(@PathVariable int id) {
 		return employeeService.getEmployeeById(id);
+	}
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void updateEmployee(@RequestBody Employee employee) {
+		employeeService.updateEmployee(employee);
+	}
+
+	@DeleteMapping(path = "/{id}")
+	public void removeEmployee(@PathVariable int id) {
+		employeeService.removeEmployee(id);
 	}
 
 }
