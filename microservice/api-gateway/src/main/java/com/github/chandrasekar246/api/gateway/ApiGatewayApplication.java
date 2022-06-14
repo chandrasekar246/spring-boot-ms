5e8713d538ae6c8bfae886b88e066b7b699ea780
Chandrasekar S <chandrasekar246@yahoo.co.in>
2022-06-14 08:11:26 +0530

    fix

--- microservice/api-gateway/src/main/java/com/github/chandrasekar246/api/gateway/ApiGatewayApplication.java
+++ microservice/api-gateway/src/main/java/com/github/chandrasekar246/api/gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.github.chandrasekar246.feign.usersvc;
+package com.github.chandrasekar246.api.gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,14 +9,19 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(ApiGatewayApplication.class);
 
 	public static void main(String[] args) {
-		
+
 		LOGGER.info("********** Application started... **********");
-		
+
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+//	@Bean
+//	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//		return builder.routes().route("banking", r -> r.path("/banking").uri("lb://BANKING-APP")).build();
+//	}
 
 }
